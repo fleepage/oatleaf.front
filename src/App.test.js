3432ae5ee378c56b96a2be174e2fb3690fbacdd7
xenophilya <manuelemeka@gmail.com>
2020-12-16 12:08:57 +0100

    first test passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("checking .h-100 css class", () => {
+  //const { getByText } = render(<App />);
+  //const linkElement = getByText(/Register Your School/i);
+
+  const { container } = render(<App />);
+  expect(container.querySelector(".h-100")).toBeInTheDocument();
 });
