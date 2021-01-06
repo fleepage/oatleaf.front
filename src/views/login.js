6f3a1d37caab5aba160c6add11ee2c386f5bde8f
xenophilya <manuelemeka@gmail.com>
2021-01-06 16:42:19 +0100

    SignIn + SignUp Integration Completed

--- src/views/login.js
+++ src/views/login.js
@@ -31,10 +31,6 @@ const validatePassword = (value) => {
 const validateEmail = (value) => {
   let error;
   if (!value) error = "Please enter your username";
-
-  // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-  //   error = "Invalid email address";
-  // }
   return error;
 };
 
